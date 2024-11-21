4aeb2408cdbfeb62a41c2ececb44face75bd53a0
Noah <nuhumusah874@gmail.com>
2024-11-21 21:16:10 +0000

    Implementing functionality using methods

--- chapter-6/PacktLibrary/Person.cs
+++ chapter-6/PacktLibrary/Person.cs
@@ -7,17 +7,21 @@ public class Person
   public DateTimeOffset Born { get; set; }
   public List<Person> Children = new();
 
-  // Allow multiple spouses to be stored for a person
+  // Allow multiples spouses to be stored for a person
   public List<Person> Spouses = new();
-  // A read-only property to show if a person is married or not
+
+  // A read-only property to show if a person is married to anyone
   public bool Married => Spouses.Count > 0;
+
   #endregion
 
+
   #region Methods
+  
   public void WriteToConsole()
   {
-    WriteLine($"{Name} was born on a {Born:dddd}");
-  } 
+    WriteLine($"{Name} was born on a {Born:dddd}.");   
+  }
 
   public void WriteChildrenToConsole()
   {
@@ -31,31 +35,32 @@ public class Person
     ArgumentNullException.ThrowIfNull(p1);
     ArgumentNullException.ThrowIfNull(p2);
 
-    if(p1.Spouses.Contains(p2) || p2.Spouses.Contains(p1))
+    if (p1.Spouses.Contains(p2) || p2.Spouses.Contains(p1))
     {
-      throw new ArgumentException(string.Format("{0} is already married to {1}", arg0: p1.Name, p2.Name));
+      throw new ArgumentException(
+        string.Format("{0} is already married to {1}", arg0: p1.Name, arg1: p2.Name));
     }
 
     p1.Spouses.Add(p2);
     p2.Spouses.Add(p1);
   }
 
-  // Instance method to marry two people
+  // Instace Method to marry another person
   public void Marry(Person partner)
   {
-    Marry(this, partner); // "this" is the current person.
+    Marry(this, partner); // "this" is the current person
   }
 
   public void OutputSpouses()
   {
     if(Married)
     {
-      string term = Spouses.Count == 1 ? "Person" : "People";
+      string term = Spouses.Count == 1 ? "person" : "people";
       WriteLine($"{Name} is married to {Spouses.Count} {term}");
 
       foreach(Person spouse in Spouses)
       {
-        WriteLine($"  {spouse.Name}");
+        WriteLine($" {spouse.Name}");
       }
     }
     else
@@ -64,15 +69,16 @@ public class Person
     }
   }
 
-  /// <summary> 
-  /// A static method to multiple "Have children" together
+  /// <summary>
+  /// Static method to "multiply" aka procreate and have a child together.
   /// </summary>
   /// <param name="p1">Parent 1</param>
   /// <param name="p2">Parent 2</param>
-  /// <returns>A person object that is a child of parent 1 and parent 2</return>
-  /// <exception cref="ArgumentNullException">If p1 or p2 are null</exception>
-  /// <exception cref="ArgumentException">If p1 and p2 are not married</exception>
-  
+  /// <returns>A Person object that is the child of Parent 1 and Parent2.</returns>
+  /// <exception cref="ArgumentNullException">If p1 or p2 are null.</exception>
+  /// <exception cref="ArgumentException">If p1 and p2 are not married.</exception>
+  /// 
+
   public static Person Procreate(Person p1, Person p2)
   {
     ArgumentNullException.ThrowIfNull(p1);
@@ -80,9 +86,11 @@ public class Person
 
     if(!p1.Spouses.Contains(p2) && !p2.Spouses.Contains(p1))
     {
-      throw new ArgumentException(string.Format("{0} must be married with {1} to procreate", arg0: p1.Name, arg1: p2.Name));
+      throw new ArgumentException(string.Format(
+        "{0} must be married to {1} to procreate with them.",
+        arg0: p1.Name, arg1: p2.Name)
+      );
     }
-
     Person baby = new()
     {
       Name = $"Baby of {p1.Name} and {p2.Name}",
@@ -95,29 +103,11 @@ public class Person
     return baby;
   }
 
-  // An Instance method to "multiply"
+  // Instance method to "multiply"
   public Person ProcreateWith(Person partner)
   {
     return Procreate(this, partner);
   }
-  #endregion
 
-  #region Operators
-
-  // Define the + operator to "Marry"
-  public static bool operator +(Person p1, Person p2)
-  {
-    Marry(p1, p2);
-
-    // Confirm the marriage
-    return p1.Married && p2.Married;
-  }  
-
-  // Define the * operator to procreate
-  public static Person operator *(Person p1, Person p2)
-  {
-    return Procreate(p1, p2);
-  }
   #endregion
 }
-
--- chapter-6/PeopleApp/Program.cs
+++ chapter-6/PeopleApp/Program.cs
@@ -2,55 +2,43 @@
 
 Person harry = new()
 {
-  Name = "Harry",
-  Born = new(year: 2000, month: 4, day: 16, hour: 11, minute: 0, second: 0, offset: TimeSpan.Zero)
+  Name="Harry",
+	Born = new(year: 2001, month: 3, day: 25, hour: 0, minute: 0, second: 0, offset: TimeSpan.Zero)
 };
 
 harry.WriteToConsole();
-harry.WriteChildrenToConsole();
 
-// Implementing functionality using methods
-Person lamech = new() { Name = "Lamech" };
-Person adah = new() { Name = "Adah" };
-Person zillah = new() { Name = "Zillah" };
+// Implementing functionality using method
+Person lamech = new() {Name = "Lamech"};
+Person adah = new() {Name = "Adah"};
+Person zillah = new() {Name = "Zillah"};
 
-// call the instance method to marry lamech to adah
+// call the instance method to marry lamech and adah
 lamech.Marry(adah);
 
-// call the static method to marry lamech and zillah
-//Person.Marry(lamech, zillah);
+// Call static method to marry lamech and zillah
+Person.Marry(lamech, zillah);
 
 lamech.OutputSpouses();
 adah.OutputSpouses();
 zillah.OutputSpouses();
 
-// Call the instance method to make a baby
+
+// call the instance method to make a baby
 Person baby1 = lamech.ProcreateWith(adah);
 baby1.Name = "Jabal";
 WriteLine($"{baby1.Name} was born on {baby1.Born}");
 
-// call static method to make a baby
-//Person baby2 = Person.Procreate(lamech, zillah);
-//baby2.Name = "Tucheliban";
+// Call the static method to make a baby
+Person baby2 = Person.Procreate(zillah, lamech);
+baby2.Name = "Tucheliban";
 
 adah.WriteChildrenToConsole();
 lamech.WriteChildrenToConsole();
 zillah.WriteChildrenToConsole();
 
-// Loop through lamech's children
 for(int i = 0; i < lamech.Children.Count; i++)
 {
-  WriteLine("{0}'s Child #{1} is named \"{2}\".", lamech.Name, i, lamech.Children[i].Name);
+	WriteLine(format: "{0}'s child #{1} is named \"{2}\".", 
+	arg0: lamech.Name, arg1: i, arg2: lamech.Children[i].Name);
 }
-
-if (lamech + zillah)
-{
-  WriteLine($"{lamech.Name} and {zillah.Name} successfully got married");
-}
-
-// use * operator to multiply
-Person baby3 = lamech * adah;
-baby3.Name = "Artenyahu";
-
-Person baby4 = zillah * lamech;
-baby4.Name = "Hansi";
